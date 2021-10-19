ba43676f37af6c8f5a4046e369d56d8787979161
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2021-10-19 15:38:45 +0900

    incomplate-list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+// テキストボックスの値を取得し、初期化する
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  // li生成
+  const li = document.createElement("li");
+  li.className = "list";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(p);
+  // console.log(li);
+  // 未完了リストに追加
+  document.getElementById("incomplate-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
